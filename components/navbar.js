3013b1965e30c4571fec0e745f992554b657ca44
Vishal Bhargava <vishuji215@gmail.com>
2022-07-23 03:38:34 +0530

    bugs fixed in navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,3 +3,24 @@ window.addEventListener("scroll",()=>{
     let position=window.scrollY>0;
     nav.classList.toggle("afterscroll", position);
 })
+
+let nav = document.querySelector('.navbar');
+let dropdown = nav.querySelector('.nv_item');
+let dropdownToggle = nav.querySelector("[data-action='dropdown-toggle']");
+let navToggle = nav.querySelector("[data-action='nav-toggle']");
+
+dropdownToggle.addEventListener('click', () => {
+	if (dropdown.classList.contains('show')) {
+		dropdown.classList.remove('show');
+	} else {
+		dropdown.classList.add('show');
+	}
+})
+
+navToggle.addEventListener('click', () => {
+	if (nav.classList.contains('opened')) {
+		nav.classList.remove('opened');
+	} else {
+		nav.classList.add('opened');
+	}
+})
